5421ea3562d012ced3143c073656de8496a71f04
randy <randydu@gmail.com>
2021-01-07 12:55:50 -0800

    adds tag-callback macros;

--- catch_ex.h
+++ catch_ex.h
@@ -4,39 +4,44 @@
 #include <functional>
 
 namespace catch_ex {
-    typedef std::function<void(bool start)> test_callback_t;
-    typedef std::function<void(bool start, const std::string& tag)> tag_callback_t;
+typedef std::function<void(bool start)> test_callback_t;
+typedef std::function<void(bool start, const std::string &tag)> tag_callback_t;
 
-    void register_test_callback(test_callback_t test_callback);
-    void register_tag_callback(const std::string& tag_or_tags, tag_callback_t tag_callback, bool shared);
+void register_test_callback(test_callback_t test_callback);
+void register_tag_callback(const std::string &tag_or_tags, bool shared, tag_callback_t tag_callback);
 
-    void enable_verbose_printing(bool enable);
-    bool verbose_printing();
+void enable_verbose_printing(bool enable);
+bool verbose_printing();
 
-    namespace {
-        typedef std::function<void()> dummy_t;
+namespace {
+typedef std::function<void()> dummy_t;
 
-        dummy_t dummy;
+dummy_t dummy;
+} // namespace
+
+struct autostart {
+    dummy_t f_;
+    dummy_t g_;
+
+    autostart(dummy_t &&f) : f_(std::move(f)) {
+        if (f_)
+            f_();
     }
+    autostart(dummy_t &&f, dummy_t &&g) : f_(std::move(f)), g_(std::move(g)) {
+        if (f_)
+            f_();
+    }
+    ~autostart() {
+        if (g_)
+            g_();
+    }
+};
 
-    struct autostart {
-        dummy_t f_;
-        dummy_t g_;
-
-        autostart(dummy_t&& f) : f_(std::move(f)) {
-            if (f_) f_();
-        }
-        autostart(dummy_t&& f, dummy_t&& g) : f_(std::move(f)), g_(std::move(g)) {
-            if (f_) f_();
-        }
-        ~autostart() {
-            if (g_) g_();
-        }
-    };
-
-    // for internal usage, published for unit-test.
-    std::vector<std::string> parse_tags(std::string const& arg);
-}
+// for internal usage, published for unit-test.
+std::vector<std::string> parse_tags(std::string const &arg);
+std::string parse_first_tag(std::string const &arg);
+bool is_same_tag(const std::string &tag1, const std::string &tag2);
+} // namespace catch_ex
 
 #ifndef COMBINE1
 #define COMBINE1(X, Y) X##Y // helper macro
@@ -46,12 +51,33 @@ namespace catch_ex {
 #define COMBINE(X, Y) COMBINE1(X, Y)
 #endif
 
-#define AUTO_START(code_start) catch_ex::autostart COMBINE(autostart, __LINE__) ( [](){ code_start; } )
+#define AUTO_START(code_start) catch_ex::autostart COMBINE(autostart, __LINE__)([]() { code_start; });
 
-#define AUTO_EXIT(code_exit) auto COMBINE(autostart, __LINE__) = catch_ex::make_autostart(dummy, [&](){ code_exit; })
+#define AUTO_EXIT(code_exit) auto COMBINE(autostart, __LINE__) = catch_ex::make_autostart(dummy, [&]() { code_exit; });
 
-#define AUTO_START_EXIT(code_start, code_exit) auto COMBINE(autostart, __LINE__) = catch_ex::make_autostart([&](){ code_start; }, [&](){ code_exit; })
+#define AUTO_START_EXIT(code_start, code_exit) auto COMBINE(autostart, __LINE__) = catch_ex::make_autostart([&]() { code_start; }, [&]() { code_exit; });
 
-#define REGISTER_TEST_CALLBACK(code_start, code_exit) AUTO_START(catch_ex::register_test_callback([&](bool start){ if(start){ code_start; } else { code_exit; } }))
+#define REGISTER_TEST_CALLBACK(cb) AUTO_START(catch_ex::register_test_callback(cb));
+#define TEST_CALLBACK(code_start, code_exit) AUTO_START(catch_ex::register_test_callback([&](bool start) { if(start){ code_start; } else { code_exit; } }));
+
+#define REGISTER_TAG_CALLBACK(tag, cb) AUTO_START(catch_ex::register_tag_callback(tag, false, cb));
+#define REGISTER_SHARED_TAG_CALLBACK(tag, cb) AUTO_START(catch_ex::register_tag_callback(tag, true, cb));
+
+#define TAG_CALLBACK_BEGIN(tag, shared) catch_ex::autostart COMBINE(autostart, __LINE__) ( [](){  catch_ex::register_tag_callback(tag, shared, [&](bool start, const std::string& tag_){ if(catch_ex::is_same_tag(tag, tag_)) {
+
+#define ON_START(code) \
+    if (start) {       \
+        code;          \
+    }
+
+#define ON_END(code) \
+    if (!start) {    \
+        code;        \
+    }
+
+#define TAG_CALLBACK_END      \
+    }                         \
+    }); \
+    });
 
 #endif
--- catch_ex_tests.cpp
+++ catch_ex_tests.cpp
@@ -1,5 +1,5 @@
-#include <catch2/catch.hpp>
 #include "catch_ex.h"
+#include <catch2/catch.hpp>
 
 #include <iostream>
 #include <sstream>
@@ -7,44 +7,39 @@
 void test_callback(bool start) {
     if (start) {
         std::cout << "test start >>" << std::endl;
-    }
-    else {
+    } else {
         std::cout << "test ended <<" << std::endl;
     }
 }
 
-void mycallback1(bool start, const std::string& tag) {
+void mycallback1(bool start, const std::string &tag) {
     if (start) {
         std::cout << "mycallback1: tag (" << tag.c_str() << ") >>" << std::endl;
-    }
-    else {
+    } else {
         std::cout << "mycallback1: tag (" << tag.c_str() << ") <<" << std::endl;
     }
 }
 
-void mycallback2(bool start, const std::string& tag) {
+void mycallback2(bool start, const std::string &tag) {
     if (start) {
         std::cout << "mycallback2: tag (" << tag.c_str() << ") >>" << std::endl;
-    }
-    else {
+    } else {
         std::cout << "mycallback2: tag (" << tag.c_str() << ") <<" << std::endl;
     }
 }
 
-void mycallback3(bool start, const std::string& tag) {
+void mycallback3(bool start, const std::string &tag) {
     if (start) {
         std::cout << "mycallback3: tag (" << tag.c_str() << ") >>" << std::endl;
-    }
-    else {
+    } else {
         std::cout << "mycallback3: tag (" << tag.c_str() << ") <<" << std::endl;
     }
 }
 AUTO_START(
     catch_ex::register_test_callback(test_callback);
-    catch_ex::register_tag_callback("tag1", mycallback1, false);
-    catch_ex::register_tag_callback("[tag2]", mycallback2, false);
-    catch_ex::register_tag_callback("tag1, tag2", mycallback3, true);
-);
+    catch_ex::register_tag_callback("tag1", false, mycallback1);
+    catch_ex::register_tag_callback("[tag2]", false, mycallback2);
+    catch_ex::register_tag_callback("tag1, tag2", true, mycallback3););
 
 TEST_CASE("test-tag1-0", "[tag1]") {
 }
@@ -76,4 +71,25 @@ TEST_CASE("parse_tags", "[catch_ex]") {
     CHECK_THAT(parse_tags("[xxx], [ yyy ],[ ]"), Catch::Matchers::Equals(std::vector<std::string>{"xxx", "yyy"}));
     CHECK_THAT(parse_tags("[xxx, yyy , ]"), Catch::Matchers::Equals(std::vector<std::string>{"xxx", "yyy"}));
     CHECK_THAT(parse_tags("[xxx, [yyy] , ]"), Catch::Matchers::Equals(std::vector<std::string>{"xxx", "yyy"}));
+}
+
+TEST_CASE("parse_tag", "[catch_ex]") {
+    using namespace catch_ex;
+
+    CHECK(parse_first_tag("xxx") == "xxx");
+    CHECK(parse_first_tag(" xxx ") == "xxx");
+    CHECK(parse_first_tag("[xxx]") == "xxx");
+    CHECK(parse_first_tag("[xxx, ]") == "xxx");
+    CHECK(parse_first_tag("[xxx, ], yyy") == "xxx");
+}
+
+TEST_CASE("same_tag", "[catch_ex]") {
+    using namespace catch_ex;
+
+    CHECK(is_same_tag("xxx", "xxx"));
+    CHECK(is_same_tag("xxx", "[xxx]"));
+    CHECK(is_same_tag("[xxx]", "xxx"));
+    CHECK(is_same_tag(" xxx ", "xxx"));
+    CHECK(is_same_tag("[xxx, ]", "xxx"));
+    CHECK(is_same_tag("[xxx, ], yyy", "xxx"));
 }
